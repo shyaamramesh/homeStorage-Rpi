031d6f8a7601ea036e6982c5351a3afbccd73035
Shyaam Ramesh <49405098+shyaamramesh@users.noreply.github.com>
2019-04-09 00:03:29 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,13 +12,13 @@ if(isset($_FILES['file'])) {
 	$f_ext = explode('.', $f_name);
 	$f_ext = strtolower(end($f_ext));
 	
-	$allowed = array('txt', 'jpg', 'png');
+	$allowed = array('txt', 'jpg', 'png'); //ALLOWED FILE TYPES
 	
 	if(in_array($f_ext, $allowed)) {
 		if($f_err === 0) {
 			if($f_size <= 2097152){
 				$f_name_new = uniqid('', true) . '.' . $f_ext;
-				$f_destination = '/var/www/html/uploads/' . $f_name_new;
+				$f_destination = '/var/www/html/uploads/' . $f_name_new; // FILE LOCATION
 				
 				if(move_uploaded_file($f_temp, $f_destination)){
 					echo $f_destination;
